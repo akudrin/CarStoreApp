3fc4a793d2adbace6efc85784d71a1950aefc152
Andrei Kudrin <andreikudrin@gmail.com>
2019-04-12 09:24:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Login from "./components/Login";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <AppBar position="static" color="default">
+          <Toolbar>CarList</Toolbar>
+        </AppBar>
+        <Login />
       </div>
     );
   }
